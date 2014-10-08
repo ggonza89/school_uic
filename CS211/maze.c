aececa69eeaae7731791653dca1f8fbfb1f88519
Giancarlo Gonzalez <ggonza89@me.com>
2014-10-08 10:49:59 -0500

    takes care of all file input errors

--- CS211/maze.c
+++ CS211/maze.c
@@ -32,23 +32,64 @@ int main (int argc, char **argv)
         exit(-1);
     }
     
-    /* read in the size, starting and ending positions in the maze */
+    /* read in the size of maze */
     fscanf (src, "%d %d", &m1.xsize, &m1.ysize);
-    fscanf (src, "%d %d", &m1.xstart, &m1.ystart);
-    fscanf (src, "%d %d", &m1.xend, &m1.yend);
-    /* print them out to verify the input */
-    printf ("size: %d, %d\n", m1.xsize, m1.ysize);
-    printf ("start: %d, %d\n", m1.xstart, m1.ystart);
-    printf ("end: %d, %d\n", m1.xend, m1.yend);
+    fprintf(stderr, "%d %d\t", m1.xsize, m1.ysize);
     
-    if(m1.xsize < 3 || m1.ysize < 3) {
+    while(m1.xsize < 3 || m1.ysize < 3) {
         
-        fprintf (stderr, "Invalid: Maze sizes must be greater than 0\n");
-        return 0;
+        fprintf (stderr, "=> Invalid: Maze sizes must be greater than 0\n");
+        fscanf(src, "%d %d", &m1.xsize, &m1.ysize);
+        fprintf(stderr, "%d %d\t", m1.xsize, m1.ysize);
         
     }
+    
+    fprintf(stderr, "=> Maze becomes size %d x %d\n", m1.xsize, m1.ysize);
+    
+    /* read in starting position of maze */
+    while(1) {
+        
+        fscanf (src, "%d %d", &m1.xstart, &m1.ystart);
+        fprintf(stderr, "%d %d\t", m1.xstart, m1.ystart);
+        
+        if(m1.xstart > m1.xsize)
+            fprintf(stderr, "=> Invalid: row %d is outside range from 1 to %d", m1.xstart, m1.xsize);
+        else if(m1.ystart > m1.ysize)
+            fprintf(stderr, "=> Invalid: column %d is outside range from 1 to %d", m1.ystart, m1.ysize);
+        else {
+            fprintf(stderr, "=> Starting position is at position %d, %d\n", m1.xstart, m1.ystart);
+            break;
+        }
+        
+        fprintf(stderr, "\n");
+        
+    }
+
+    /* read in ending position of maze */
+    while(1) {
+        
+        fscanf (src, "%d %d", &m1.xend, &m1.yend);
+        fprintf(stderr, "%d %d\t", m1.xend, m1.yend);
+        
+        if(m1.xend > m1.xsize)
+            fprintf(stderr, "=> Invalid: row %d is outside range from 1 to %d", m1.xend, m1.xsize);
+        else if(m1.yend > m1.ysize)
+            fprintf(stderr, "=> Invalid: column %d is outside range from 1 to %d", m1.yend, m1.ysize);
+        else {
+            fprintf(stderr, "=> Ending position is at position %d, %d\n", m1.xend, m1.yend);
+            break;
+        }
+        
+        fprintf(stderr, "\n");
+        
+    }
+    
+    /* print them out to verify the input */
+//    printf ("size: %d, %d\n", m1.xsize, m1.ysize);
+//    printf ("start: %d, %d\n", m1.xstart, m1.ystart);
+//    printf ("end: %d, %d\n", m1.xend, m1.yend);
+    
     /* allocate the maze */
-    printf ("allocating maze...\n");
     m1.arr = (char **) malloc (sizeof(char *) * (m1.xsize+2));
     for (i = 0; i < m1.xsize+2; i++)
         m1.arr[i] = (char *) malloc (sizeof(char ) * (m1.ysize+2));
@@ -77,7 +118,22 @@ int main (int argc, char **argv)
     /* mark the blocked positions in the maze with *'s */
     while (fscanf (src, "%d %d", &xpos, &ypos) != EOF)
     {
-        m1.arr[xpos][ypos] = '*';
+        
+        fprintf(stderr, "%d %d\t", xpos, ypos);
+        // if xpos or ypos not in bounds, skip that input line
+        if(xpos > m1.xsize)
+            fprintf(stderr, "=> Invalid: row %d is outside range from 1 to %d", xpos, m1.xsize);
+        else if(ypos > m1.ysize)
+            fprintf(stderr, "=> Invalid: column %d is outside range from 1 to %d", ypos, m1.ysize);
+        // if (xpos,ypos) is the starting point, skip input line
+        else if(xpos == m1.xstart && ypos == m1.ystart)
+            fprintf(stderr, "=> Invalid: attempting to block starting position");
+        // default is to mark spot as blocked
+        else
+            m1.arr[xpos][ypos] = '*';
+        
+        fprintf(stderr, "\n");
+        
     }
     
     /* print out the initial maze */
